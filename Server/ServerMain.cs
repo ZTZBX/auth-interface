f23894889862a1db1d07a6015ea3a99392a46165
zabbix-byte <zabbix@ztrunk.space>
2023-04-17 14:20:27 +0200

    Añadiendo base

---
+++ Server/ServerMain.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace auth_interface.Server
+{
+    public class ServerMain : BaseScript
+    {
+        public ServerMain()
+        {
+        }
+
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+file 'Client/*.dll'
+
+client_script 'Client/*.net.dll'
+server_script 'Server/*.net.dll'
+
+author 'zabbix-byte'
+version '1.0.0'
+description 'ztzbx auth interface'
